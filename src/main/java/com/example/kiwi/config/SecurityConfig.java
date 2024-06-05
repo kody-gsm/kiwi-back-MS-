e9486a005e6379e80d70105d915fc55b1223a34c
minsung gwak <131960511+opinionist@users.noreply.github.com>
2024-06-05 15:37:00 +0900

    securityConfig 수정

--- src/main/java/com/example/kiwi/config/SecurityConfig.java
+++ src/main/java/com/example/kiwi/config/SecurityConfig.java
@@ -1,33 +1,29 @@
 package com.example.kiwi.config;
 
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig{
+public class SecurityConfig {
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/*").and();
-    }
-
-    @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .formLogin(form -> form.loginPage("http://localhost:3000/").permitAll());
-        http
-                .logout(log -> log.logoutSuccessUrl("/"));
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)//csrf 공격 꺼두기
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers("/asdf").authenticated();
+                    authorize.anyRequest().permitAll();
+                })
+                .formLogin((formlogin) -> {
+//                    formlogin.loginPage("http://localhost:3000/login");
+                })
+                .build();
     }
 
     @Bean
